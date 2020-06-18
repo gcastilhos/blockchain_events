c1914074ca7a8ceeee03a2c4ba5b1468a2132b49
gcastilhos <gcastilhos@gmail.com>
2020-06-18 22:51:20 +1000

    Updated split_data.py

--- data2/split_data.py
+++ data2/split_data.py
@@ -56,6 +56,10 @@ def calculate_total(data, index):
 
 def create_record_groups(data):
     data.loc[data['USE CATEG'].isnull(), ['USE CATEG', 'CATEGORY NAME (Literal)']] = ('UNDETM', 'UNDETM')
+<<<<<<< Updated upstream
+=======
+    data['CATEGORY NAME (Literal)'] = data['CATEGORY NAME (Literal)'].apply(lambda val: val.strip())
+>>>>>>> Stashed changes
     for counter in range(0, data.shape[0], 100):
         data_slice = data.iloc[slice(counter, counter + 100)]
         index = int(counter / 100) + 1
